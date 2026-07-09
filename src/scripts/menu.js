5ff4783aafda88e8d5f86cdb3c2b9ae663ac5887
Haneen <haninmohamed@graduate.utm.my>
2026-07-09 22:49:36 +0300

    fixed responsive header design for bigger screens

---
+++ src/scripts/menu.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('.hamburger-menu');
+
+menu?.addEventListener('click', () => {
+  const isExpanded = menu.getAttribute('aria-expanded') === 'true';
+  menu.setAttribute('aria-expanded', `${!isExpanded}`);
+});
