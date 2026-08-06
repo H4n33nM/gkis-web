cc5f0cb2eca5d8ef840afd108e30d726f16516e4
Haneen <haninmohamed@graduate.utm.my>
2026-08-06 19:42:35 +0300

    fixed image slider functionality

--- src/scripts/imageSlider.js
+++ src/scripts/imageSlider.js
@@ -1,5 +1,7 @@
 let slideIndex = 1;
 showSlides(slideIndex);
+document.querySelector(".prev").addEventListener("click", () => plusSlides(-1));
+document.querySelector(".next").addEventListener("click", () => plusSlides(1));
 
 function plusSlides(n) {
   showSlides((slideIndex += n));
@@ -11,17 +13,15 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName(".slideGroups");
-  let dots = document.getElementsByClassName(".dot");
+  let slides = document.getElementsByClassName("slideGroups");
   if (n > slides.length) {
     slideIndex = 1;
   }
   if (n < 1) {
     slideIndex = slides.length;
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += "active";
 }
